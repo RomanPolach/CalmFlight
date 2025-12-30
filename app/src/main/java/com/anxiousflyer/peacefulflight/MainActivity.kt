cd064e150e58d7e1a0ac95b73701b01c3543ae92
Roman <roman.polach@cyberfox.cz>
2025-12-30 16:55:42 +0100

    Polish splash screen: white background in light mode and circular launcher icon

--- app/src/main/java/com/anxiousflyer/peacefulflight/MainActivity.kt
+++ app/src/main/java/com/anxiousflyer/peacefulflight/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -15,6 +16,7 @@ import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
